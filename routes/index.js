31368275d32defd4c88a7ce577990cc2ed8d3f17
Di Jiao <v-jiaodi@microsoft.com>
2021-12-06 10:03:08 +0800

    update sample with @azure/storage-blob

--- routes/index.js
+++ routes/index.js
@@ -5,47 +5,40 @@ if (process.env.NODE_ENV !== 'production') {
 const
       express = require('express')
     , router = express.Router()
-    , azureStorage = require('azure-storage')
-    , blobService = azureStorage.createBlobService()
+    , { BlobServiceClient } = require("@azure/storage-blob")
+    , blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
     , containerName = 'thumbnails'
     , config = require('../config')
 ;
 
-router.get('/', (req, res, next) => {
-
-  blobService.listBlobsSegmented(containerName, null, (err, data) => {
-
-    let viewData;
-
-    if (err) {
-
-      viewData = {
-        title: 'Error',
-        viewName: 'error',
-        message: 'There was an error contacting the blob storage container.',
-        error: err
-      };
-      
-      res.status(500);
-
-    } else {
-
+router.get('/', async(req, res, next) => {
+  let viewData;
+  try{
+      const blobs = blobServiceClient.getContainerClient(containerName).listBlobsFlat()
       viewData = {
         title: 'Home',
         viewName: 'index',
         accountName: config.getStorageAccountName(),
-        containerName: containerName
+        containerName: containerName,
+        thumbnails:[]
       };
-
-      if (data.entries.length) {
-        viewData.thumbnails = data.entries;
+      for await(let blob of blobs){
+          viewData.thumbnails.push(blob);
       }
-      
+    
+    }catch(err){
+      viewData = {
+          title: 'Error',
+          viewName: 'error',
+          message: 'There was an error contacting the blob storage container.',
+          error: err
+        };
+        
+        res.status(500);
     }
-
-    res.render(viewData.viewName, viewData);
-  });
-
+  res.render(viewData.viewName, viewData);
 });
+
+
 
 module.exports = router;
