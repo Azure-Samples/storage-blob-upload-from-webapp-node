8a6862bc01735854a698eb8063b5e3bb6cdea646
Natalia Venditto <nataliafrontend@gmail.com>
2022-10-09 22:15:03 +0200

    make container name dynamic

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const
     , router = express.Router()
     , { BlobServiceClient } = require("@azure/storage-blob")
     , blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
-    , containerName = 'thumbnails'
+    , containerName = process.env.AZURE_STORAGE_CONTAINER_NAME
     , config = require('../config')
 ;
 
